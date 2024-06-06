6150f3f3fad15898004d9e5ecf2e9727c1be5b68
liuqi <liuqi@yimian.com.cn>
2024-06-06 17:46:06 +0800

    replace: whole project

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,23 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from "vite-plugin-static-copy";
-import path from "path";
-import filePlugin from "./plugins/filePlugin";
+import { crx } from '@crxjs/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import manifest from './src/manifest'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "http://localhost:5173/",
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './manifest.json',
-          dest: '/dev.dist'
-        }
-      ]
-    }),
-    filePlugin()
-  ],
-  server: {
-    host: true,
-    port: 8000,
-    watch: {
-      ignored: ['src/template/**/*']
-    }
-  },
-  resolve: {
-    alias: [
-      {
-        find: /^@t\//,
-        replacement: path.resolve(__dirname, 'src/template') + '/'
-      }, // 模块路径alias
-    ]
+export default defineConfig(({ mode }) => {
+  const production = mode === 'production'
+
+  return {
+    build: {
+      cssCodeSplit: true,
+      emptyOutDir: true,
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/chunk-[hash].js',
+        },
+      },
+    },
+    plugins: [crx({ manifest }), vue()],
   }
 })
